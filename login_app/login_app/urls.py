b7a584ea2336f7f13e8081e8f6ebbfbedbccd9fa
Le Thanh Tien <TienLT7@fsoft.com.vn>
2019-08-15 11:23:09 +0700

    add login page

--- login_app/login_app/urls.py
+++ login_app/login_app/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path ( '', TemplateView.as_view ( template_name='home.html' ), name='home' ),
+    path ( 'login/', auth_views.LoginView.as_view (), name='login' ),
+    path ( 'logout/', auth_views.LogoutView.as_view (), name='logout' )
 ]
